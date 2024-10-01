f6aa907aed7b857eafebf3b0485ded62c824819d
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-01 20:24:33 -0300

    Recorriendo la lista de pacientes del store de Zustand en PatientList y componentes para detalles de paciente e items

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,4 +1,5 @@
 import { usePatientStore } from "../store";
+import PatientDetails from "./PatientDetails";
 
 export default function PatientList() {
 
@@ -7,6 +8,30 @@ export default function PatientList() {
   console.log(patients)
 
   return (
-  <div>PatientList</div>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+        {patients.length ? (
+            <>
+                <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+                <p className="text-xl mt-5 mb-10 text-center">
+                    Administra tus {""}
+                    <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+                </p>
+                {patients.map(patient => (
+                    <PatientDetails 
+                        key={patient.id}
+                        patient={patient}
+                    />
+                ))}
+            </>
+        ) : (
+            <>
+                <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                <p className="text-xl mt-5 mb-10 text-center">
+                    Comienza agregando pacientes {""}
+                    <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+                </p>
+            </>
+        )}
+    </div>
   )
 }
