192e159d95ba373b53ae236f433dedcac9287729
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-02 20:17:05 -0300

    Agregada funcionalidad para editar paciente

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -2,22 +2,43 @@ import { useForm } from "react-hook-form";
 import Error from "./Error";
 import { DraftPatient } from "../types";
 import { usePatientStore } from "../store";
+import { useEffect } from "react";
 
 export default function PatientForm() {
 
-  const addPatient = usePatientStore(state => state.addPatient);
+  const addPatient = usePatientStore(state => state.addPatient)
+  const activeId = usePatientStore(state => state.activeId)
+  const patients = usePatientStore(state => state.patients)
+  const updatePatient = usePatientStore(state => state.updatePatient)
+
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
     reset
   } = useForm<DraftPatient>();
 
+  useEffect(() => {
+    if(activeId) {
+      const activePatient = patients.filter( patient => patient.id === activeId )[0]
+      setValue("name", activePatient.name)
+      setValue("caretaker", activePatient.caretaker)
+      setValue("date", activePatient.date)
+      setValue("email", activePatient.email)
+      setValue("symptoms", activePatient.symptoms)
+    }
+  }, [activeId])
+
   const registerPatient = (data: DraftPatient) => {
-    addPatient(data)
+    if(activeId) {
+      updatePatient(data)
+    } else {
+      addPatient(data)
+    }
 
     reset()
-  };
+  }
 
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5">
